0a87cd1315e78f9003d2882b585d7e6b11e2fa00
Nishan pubudu kumara <165471638+NPK14622@users.noreply.github.com>
2024-08-23 02:34:26 +0530

    Light managing system files uploaded

---
+++ RemoteControl.java
@@ -0,0 +1,21 @@
+public class RemoteControl {
+    private Command[] onCommands;
+    private Command[] offCommands;
+    private Command undoCommand;
+    private int slot;
+    public void setCommand(int slot,Command onCommand,Command offCommand){
+        onCommands[slot] = onCommand;
+        offCommands[slot] = offCommand;
+    }
+    public void onButtonWasPressed(int slot){
+        onCommands[slot].execute();
+        undoCommand = onCommands[slot];
+    }
+    public void offButtonWasPressed(int slot){
+        offCommands[slot].execute();
+        undoCommand = offCommands[slot];
+    }
+    public void undoButtonWasPressed(){
+        undoCommand.undo();
+    }
+}
